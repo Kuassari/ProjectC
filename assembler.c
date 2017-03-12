b08436434ef1b6da632e69fba706bf3b85609908
BurnEmDown <amitreuveni@gmail.com>
2017-03-12 17:00:30 +0200

    wrote basic for dealing with command-line arguments of file names

--- assembler.c
+++ assembler.c
@@ -1,11 +1,51 @@
 #include "common.h"
 
+void startLoop(FILE *, char *) 
+
 int main(int argc, char *argv[]) {
 
+	int i;
+	int IC = 100;
+	int DC = 0;
+	*char[] nameList;
+	
+	/* create array to hold given filenames */
+	nameList = malloc((argc) * sizeof(char*));
+	if(nameList == NULL)
+	{
+	   printf("error, out of memory");
+	   exit(0);
+	}
+	/* in each array block, create a string to hold the filename, adding ".as" at the end, as needed */
+	for(i=1;i<argc;i++){
+	   nameList[i] = malloc((MAX_FILENAME) * sizeof(char));
+	   if(nameList[i] == NULL)
+	   {
+	   printf("error, out of memory");
+	   exit(0);
+	   }
+	   strcpy(nameList[i],argv[i]);
+	   strcar(nameList[i],".as");
+	   /* copy the given filename to the name array, and add ".as" to it's end */
+	}
+	
+	for(i=1;i<argc;i++){
+	startLoop(nameList[i]);
+	endLoop(nameList[i]);
+	}
+}
 
 
 
+/* the first loop that opens the given filename, reads through it, adding the relevant information to symbols linked-list, code table, and data table */
+startLoop(char * fileName){
 
-
-
+	FILE *fp;
+	int j;
+	fp = fopen(fileName, "r");
+	if(!fp){
+	   printf("cannot open file\n");
+	   exit(0);
+	}
+	
 }
