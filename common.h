865fa72ad389163abf5ab936a47461e2aab1bda3
BurnEmDown <amitreuveni@gmail.com>
2017-03-12 17:01:04 +0200

    Update common.h

--- common.h
+++ common.h
@@ -4,3 +4,13 @@
 #include <ctype.h>
 
 #define MAX_BUF 80
+#define MAX_FILENAME 32
+
+typedef enum{relocatable,external}rel_ex;
+
+typedef struct symbols{
+	char * name;
+	rel_ex type;
+	int value;
+	*symbol next;
+}
