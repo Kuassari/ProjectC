a6f929f4d923e21f64cb718a5cfdfd1ebd7e476e
BurnEmDown <amitreuveni@gmail.com>
2017-03-17 17:45:27 +0200

    Update common.h

--- common.h
+++ common.h
@@ -6,11 +6,63 @@
 #define MAX_BUF 80
 #define MAX_FILENAME 32
 
-typedef enum{relocatable,external}rel_ex;
+typedef enum{action,instruction}act_inst;
 
 typedef struct symbols{
 	char * name;
-	rel_ex type;
-	int value;
+	char * address;
+	bool isExtern;
+	act_inst type;
 	*symbol next;
+}sym;
+
+sym _head = NULL;
+
+void addSymbol(){
+   if(_head == NULL){
+	_head = newsym;
+	newsym->next = NULL;
+   }
+   else{
+	sym temp = _head;
+	while(temp->next!=NULL)
+		temp = temp->next;
+	
+	temp->next = newsym;
+   }
+   return;	
+}
+
+void freeList(){
+   sym temp = _head;
+   while(_head != NULL)
+   {
+	_head = head->next;
+	free(temp);
+	temp=_head;
+   }	
+}
+
+int getLabel(char* labelName){
+   int count = 0;
+   sym temp = _head;
+   while(_head != NULL){
+	if(strcmp(labelName, temp->name) == 0){
+	   return count;
+	}
+	else{
+	   count++;
+	   temp = temp->next;
+	}
+   }
+   return -1;
+}
+
+char* getAddress(int position){
+   sym temp = _head;
+   int count = 0;
+   while(count < position){
+	temp = temp->next;
+   }
+   return temp->address;
 }
