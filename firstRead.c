b69f76c38464072ae2451d63a40a7b2af44aa21e
BurnEmDown <amitreuveni@gmail.com>
2017-03-18 18:54:00 +0200

    Update firstRead.c

--- firstRead.c
+++ firstRead.c
@@ -1,16 +1,5 @@
-/* check if a given string is a command. 
- * return its number if it is, or -1 if it's not */
-int checkCMD(char * toCheck)
-{
-   int i;
-   for(i = 0; i<COMMANDS_NUMBER; i++){
-	if(strcmp(commands[i],toCheck) == 0)
-		return i;
-   }
+int checkCMD(char *)
 
-
-   return -1;
-}
 
 
 /* the first loop that opens the given filename, reads through it, adding the relevant information to symbols linked-list, code table, and data table */
@@ -21,8 +10,9 @@ startLoop(char * fileName)
 	int j;
 	char[MAX_BUF] buf;
 	bool label;
-	char[MAX_LABEL] word;
+	char[MAX_LABEL_LENGTH] word;
 	char letter;
+	int labelpos;
 	int cmdNUM;		/* what comand it is */
 	fp = fopen(fileName, "r");
 
@@ -32,18 +22,28 @@ startLoop(char * fileName)
 	   exit(0);
 	}
 
-	while(fgets(buf,MAX_BUF,fp) != NULL){ /* reads a line from the text file */
-	   if(strchr(buf,':') != NULL){ /* check if the line contains a label */
+	while(fgets(buf,MAX_BUF,fp) != NULL) /* reads a line from the text file */
+	{
+	   strncpy(letter,word,1);
+	   if(letter == ';')
+	   {
+		break;
+	   }
+
+	   if((labelpos = strchr(buf,':')) != NULL)  /* check if the line contains a label */
+	   {
 	     	label = true;
 		sscanf(buf, " %s", word);
 		
-		if(strchr(word,':') == NULL){
+		if(strchr(word,':') == NULL)
+		{
 
 		    /*error, ':' is not part of label*/
 		    /* ADD ERROR HANDLING */
 		}
-		strncpy(letter,word,1);
-		if(isalpha(letter)==0){
+		
+		if(isalpha(letter)==0)
+		{
 		    /*error, first letter is not a real letter */
 		    /* ADD ERROR HANDLING */
 		}
@@ -64,26 +64,56 @@ startLoop(char * fileName)
 		}
 		
 		/* step 11 */
+	   if(label)
+	   {
+		
+		while(isspace(buf+labelpos))
+		   labelpos++;
+		word = strncpy(word,buf+labelpos,3);
+	   }
+	   else
+	   {
+		while(isspace(buf+j))
+		   j++;
+		word = strncpy(word,buf+j,3);
+	   }
+	   
 
-	   cmdNUM = checkCMD(buf);
+	   cmdNUM = checkCMD(word);
 	   switch(cmdNUM){
 		case -1: /* error: no command */
-		case 0:  /* mov */
+		case 0:  /* mov */ 
+			
 		case 1:  /* cmp */
+			
 		case 2:  /* add */
+			
 		case 3:  /* sub */
+			
 		case 4:  /* not */
+			
 		case 5:  /* clr */
+			
 		case 6:  /* lea */
+			
 		case 7:  /* inc */
+			
 		case 8:  /* dec */
+			
 		case 9:  /* jmp */ 
+			
 		case 10: /* bne */
+			
 		case 11: /* red */
+			
 		case 12: /* prn */
+			
 		case 13: /* jsr */
+			
 		case 14: /* rts */
+			
 	   	case 15: /* stop */
+			
 	   }
 	}
 
