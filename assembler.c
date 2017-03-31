5a701f2552df5d25273b237339f94ad7efb2f2fa
BurnEmDown <amitreuveni@gmail.com>
2017-03-31 18:03:22 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -11,29 +11,26 @@ int main(int argc, char *argv[])
 	nameList = (char**) malloc((argc) * sizeof(char*));
 	if(nameList == NULL)
 	{
-	   printf("error, out of memory");
-	   exit(0);
+		printf("error, out of memory");
+		exit(0);
 	}
-
-	/* in each array block, create a string to hold the filename, 
-         * adding ".as" at the end, as needed */
-	for(i=1;i<argc;i++)
+	
+	for(i=1;i<argc;i++)	  	/* copy the given filenames to the name array */
 	{
 	   nameList[i] = malloc((MAX_FILENAME) * sizeof(char));
 	   if(nameList[i] == NULL)
 	   {
-	   printf("error, out of memory");
-	   exit(0);
+	   	printf("error, out of memory");
+	   	exit(0);
 	   }
 	   strcpy(nameList[i],argv[i]);
-	   strcar(nameList[i],".as");
-	   /* copy the given filename to the name array, and add ".as" to it's end */
+
 	}
-	
+
 	for(i=1;i<argc;i++)
 	{
 		startLoop(nameList[i]);
-		endLoop(nameList[i]);
+		endLoop(nameList2[i]);
 	}
 }
 
